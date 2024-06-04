2f9c2dfe0821b08eb9fe377a1f50eee0cd791d1f
ViCode321 <110791992+ViCode321@users.noreply.github.com>
2024-06-04 17:41:02 -0600

    Login, signin y logout iniciados

--- app/models.py
+++ app/models.py
@@ -1,33 +1,21 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     gender_choices = [
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Prefer not to say'),
     ]
-    profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)    
     location = models.CharField(max_length=100, blank=True, null=False)
+    biography = models.TextField(blank=True, null=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
     gender = models.CharField(max_length=10, choices=gender_choices)
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='custom_user_groups',  # Unique related_name
-        blank=True,
-        help_text='The groups this user belongs to.',
-        related_query_name='user',
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='custom_user_permissions',  # Unique related_name
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_query_name='user',
-    )
-
     def __str__(self):
-        return f'{self.username} ({self.location or "N/A"})'
+        return f'{self.username} ({self.location or "N/A"})'    
+
 
 
 class Author(models.Model):
@@ -54,7 +42,7 @@ class Book(models.Model):
         return self.title
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -63,7 +51,7 @@ class Comment(models.Model):
         return f'Comment by {self.user.username} on {self.book.title}'
 
 class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='ratings')
     stars = models.PositiveIntegerField(default=1)
     review = models.TextField(blank=True, null=True)
